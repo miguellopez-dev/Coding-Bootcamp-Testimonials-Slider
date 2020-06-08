d588ebd4f76d589fa499d4469c2e53e4154539ad
miguel lopez <miguellopez@users.noreply.github.com>
2020-06-07 17:36:56 -0700

    Change animation to swipe and made whole layout grid

--- index.js
+++ index.js
@@ -2,40 +2,29 @@ const firstPicture = document.querySelector('.top-container1');
 const firstText = document.querySelector('.comment1');
 const secondPicture = document.querySelector('.top-container2');
 const secondText = document.querySelector('.comment2');
-const btn = document.querySelectorAll('.buttons');
 
-btn.forEach((item) => {
-	item.addEventListener('click', function () {
-		if (
-			// Check if the first set has the hide class
-			firstPicture.classList.contains('hide') &&
-			firstText.classList.contains('hide')
-		) {
-			// Adds hide class to second set
-			secondPicture.classList.add('hide');
-			secondText.classList.add('hide');
+const first = [firstPicture, firstText];
+const second = [secondPicture, secondText];
 
-			// Removes hide class from first set
-			firstPicture.classList.remove('hide');
-			firstText.classList.remove('hide');
-			// Adds Animation to first set
-			firstPicture.classList.add('animate');
-			firstText.classList.add('animate');
-		} else if (
-			// Checks if second set has the hide class
-			secondPicture.classList.contains('hide') &&
-			secondText.classList.contains('hide')
-		) {
-			// Adds hide class to first set
-			firstPicture.classList.add('hide');
-			firstText.classList.add('hide');
+const right = document.querySelector('.top-container1 .right');
+const left = document.querySelector('.top-container2 .left');
 
-			// Removes hide class from secons set
-			secondPicture.classList.remove('hide');
-			secondText.classList.remove('hide');
-			// Adds Animation to first set
-			secondPicture.classList.add('animate');
-			secondText.classList.add('animate');
-		}
+right.addEventListener('click', () => {
+	first.forEach(function (item, index) {
+		first[index].style.transform = 'translateX(-200%)';
+	});
+
+	second.forEach(function (item, index) {
+		second[index].style.transform = 'translateX(0)';
+	});
+});
+
+left.addEventListener('click', () => {
+	second.forEach(function (item, index) {
+		second[index].style.transform = 'translateX(300%)';
+	});
+
+	first.forEach(function (item, index) {
+		first[index].style.transform = 'translateX(0)';
 	});
 });
